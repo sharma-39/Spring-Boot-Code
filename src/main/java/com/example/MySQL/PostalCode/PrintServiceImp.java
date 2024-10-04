a8c94f3896bbe4c670a9ad55f525a9808c44a715
sharma-39 <55731879+sharma-39@users.noreply.github.com>
2024-10-04 10:57:09 +0530

    s

--- src/main/java/com/example/MySQL/PostalCode/PrintServiceImp.java
+++ src/main/java/com/example/MySQL/PostalCode/PrintServiceImp.java
@@ -1,40 +1,32 @@
 package com.example.MySQL.PostalCode;
-import org.apache.poi.xwpf.usermodel.XWPFDocument;
-import com.itextpdf.kernel.pdf.PdfWriter;
-import com.itextpdf.layout.Document;
-import com.itextpdf.layout.element.Paragraph;
-import com.itextpdf.layout.element.Text;
-import org.apache.poi.xwpf.usermodel.XWPFParagraph;
+
 import org.springframework.stereotype.Service;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
 
 @Service
 public class PrintServiceImp {
     public byte[] convertDocxToPdf(byte[] docxBytes) {
-        try (InputStream docxInputStream = new ByteArrayInputStream(docxBytes);
-             ByteArrayOutputStream pdfOutputStream = new ByteArrayOutputStream()) {
-
-            XWPFDocument docxDocument = new XWPFDocument(docxInputStream);
-
-            // Initialize iText PDF writer
-            PdfWriter writer = new PdfWriter(pdfOutputStream);
-            Document pdfDocument = new Document(new com.itextpdf.kernel.pdf.PdfDocument(writer));
-
-            // Convert DOCX to PDF
-            for (XWPFParagraph paragraph : docxDocument.getParagraphs()) {
-                String text = paragraph.getText();
-                pdfDocument.add(new Paragraph(text));
-            }
-
-            pdfDocument.close();
-
-            return pdfOutputStream.toByteArray();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+//        try (InputStream docxInputStream = new ByteArrayInputStream(docxBytes);
+//             ByteArrayOutputStream pdfOutputStream = new ByteArrayOutputStream()) {
+//
+//            XWPFDocument docxDocument = new XWPFDocument(docxInputStream);
+//
+//            // Initialize iText PDF writer
+//            PdfWriter writer = new PdfWriter(pdfOutputStream);
+//            Document pdfDocument = new Document(new com.itextpdf.kernel.pdf.PdfDocument(writer));
+//
+//            // Convert DOCX to PDF
+//            for (XWPFParagraph paragraph : docxDocument.getParagraphs()) {
+//                String text = paragraph.getText();
+//                pdfDocument.add(new Paragraph(text));
+//            }
+//
+//            pdfDocument.close();
+//
+//            return pdfOutputStream.toByteArray();
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
+        return "".getBytes();
     }
 }
